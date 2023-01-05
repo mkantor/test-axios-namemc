93e149a2330b4f086e66581a31adf146000867f3
Matt Kantor <the.matt.kantor@gmail.com>
2023-01-04 20:18:53 -0500

    Use Axios to request from s.namemc.com

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-console.log('Hello, world!')
+import axios from 'axios'
+
+async function debug() {
+  await axios
+    .get('https://s.namemc.com/2d/skin/face.png?id=02a3b3079e99fd6f&scale=16', {
+      responseType: 'stream',
+      validateStatus: () => true,
+    })
+    .then(console.info)
+    .catch(console.error)
+}
+
+debug()
